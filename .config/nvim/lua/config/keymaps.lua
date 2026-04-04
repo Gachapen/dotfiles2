ff1dac35355f9797c0c0eb06b83bb33317ab19b8
Magnus Bjerke Vik <post@magnusbv.no>
2026-04-04 18:44:59 +0200

    Configure rust commands in nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Keymap to run 'cargo run' using Snacks.terminal
+vim.keymap.set("n", "<leader>rr", function()
+  Snacks.terminal.open("cargo run", { cwd = LazyVim.root() })
+end, { desc = "Cargo Run (Snacks)" })
+
+-- This opens a terminal toggle; you can type whatever cargo command you want
+vim.keymap.set("n", "<leader>rt", function()
+  Snacks.terminal.toggle()
+end, { desc = "Terminal Toggle" })
