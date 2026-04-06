56ebe514ffa5b13d451d5cf8b095685fe9f4b0b6
Magnus Bjerke Vik <post@magnusbv.no>
2026-04-06 08:47:57 +0200

    Make cargo run in nvim only close on success

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,10 +2,24 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Keymap to run 'cargo run' using Snacks.terminal
 vim.keymap.set("n", "<leader>rr", function()
-  Snacks.terminal.open("cargo run", { cwd = LazyVim.root() })
-end, { desc = "Cargo Run (Snacks)" })
+  -- 1. Open the terminal with auto_close disabled
+  local term = Snacks.terminal.open("cargo run", {
+    cwd = LazyVim.root(),
+    auto_close = false,
+  })
+
+  -- 2. Attach a listener to the "TermClose" event
+  term:on("TermClose", function()
+    -- vim.v.event.status is the exit code (0 = success)
+    if vim.v.event.status == 0 then
+      term:close()
+    else
+      -- Optional: Notify that it failed if you want a heads-up
+      vim.notify("Cargo Run failed with exit code " .. vim.v.event.status, vim.log.levels.ERROR)
+    end
+  end)
+end, { desc = "Cargo Run (Success only)" })
 
 -- This opens a terminal toggle; you can type whatever cargo command you want
 vim.keymap.set("n", "<leader>rt", function()
